d9e94a162223442c7a31b0537df49b861933fe42
Fawaz Adewole <Fadewole5430@conestogac.on.ca>
2026-03-28 23:56:37 -0400

    FBA-1: add JWT auth middleware and protected route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./presentation/routes/authRoutes";
+import userRoutes from "./presentation/routes/userRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/users", userRoutes);
 
 app.get("/", (_req, res) => {
   res.send("API is running 🚀");
